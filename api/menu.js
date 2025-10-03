63b45f331478db8f4e03344cecf216d4ac5c0eea
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-10-03 15:32:12 +0530

    updated menu.js

--- api/menu.js
+++ api/menu.js
@@ -2,12 +2,24 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  const { restaurantId } = req.query;
+  const { restaurantId, lat, lng } = req.query;
 
-  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.83730&lng=80.91650&restaurantId=${restaurantId}`;
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        Referer: "https://www.swiggy.com/restaurants",
+        Platform: "dweb",
+        DNT: "1",
+      },
+    });
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*"); // Allow CORS
     res.status(200).json(data);
