d101f9ecb72ec54644faade98f5b98ae652e230c
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-10-23 00:46:20 +0530

    changed cookie

--- api/menu.js
+++ api/menu.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Firefox/144.0",
         Accept: "*/*",
-        "Cookie": `__SW=mwooOs6JBP1L7Bc0CC17NQpgROJ6ZXo0; _device_id=${deviceId}; _guest_tid=${tid}; _sid=nkn763c4-844d-4e9d-bd94-d8e1d17478ed; userLocation={%22lat%22:%22${lat}%22%2C%22lng%22:%22${lng}%22%2C%22address%22:%22%22%2C%22area%22:%22%22%2C%22showUserDefaultAddressHint%22:false}; fontsLoaded=1; aws-waf-token=121d00f9-7e4e-43e4-8292-d162c72986dd:HgoAYgOA1UWuAAAA:CDYk66B3a2ImZ/kqFzkilc/DVHvRMJ14JLNnK4GiJPBpzMnDosVnvhK93OZ3D5RtfyMXzWpX7SfAfYN76TPo2Zjos0/ZurfEqVayTvjDU/SdELwO6KUt4S1abm0aRDMs7MI4QrZddlqbgzXJwwu9Bkz2QKFHg4WK92j5Pvd0TJ7dN9/3ys9YSRTcmU7cejA=`
+        "Cookie": `__SW=mwooOs6JBP1L7Bc0CC17NQpgROJ6ZXo0; _device_id=ff4d8ba4-3ece-28d2-5b58-cde3005e1f8b; _guest_tid=9ed02fe3-edeb-4385-96d7-5ea0d475c9ac; _sid=nkn763c4-844d-4e9d-bd94-d8e1d17478ed; userLocation={%22lat%22:%2226.83730%22%2C%22lng%22:%2280.91650%22%2C%22address%22:%22%22%2C%22area%22:%22%22%2C%22showUserDefaultAddressHint%22:false}; fontsLoaded=1; aws-waf-token=121d00f9-7e4e-43e4-8292-d162c72986dd:HgoAcGyHOH8bAAAA:qsXuaEQHQ20JUeknV9A4SweySXWJIXiCrFoQg/zWjJemIQsPU+QAqL0uVm3DXAQmTLuOolk41OfUaptG06bPTgWYkaC00VfYA19XOeak0qUJuI6g3PmDtwbU3TIqYp7xP3kRhOkvSo9UTIs6Mp8I6lLJeEVSCW1AknXP71zwGM/9G/nKAcmsGQGlhnHbQXM=`
       },
     });
     const data = await response.json();
