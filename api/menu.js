b06ffea198c9af5f4691a3e759447036734a7645
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-10-23 00:38:13 +0530

    fixed url

--- api/menu.js
+++ api/menu.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
   const { resId, lat, lng } = req.query;
 
-  const resData = await fetch(`/api/restaurant?lat=${lat}&lng=${lng}`);
+  const resData = await fetch(`https://namaste-food-ashen.vercel.app/api/restaurant?lat=${lat}&lng=${lng}`);
   const json = await resData.json();
   const tid = json?.tid;
   const sid = json?.sid;
