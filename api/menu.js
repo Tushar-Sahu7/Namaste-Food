0fb12f6e4398b2431955021f1c57801d1f78c206
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-11-02 02:53:34 +0530

    fix: menu api error

--- api/menu.js
+++ api/menu.js
@@ -7,12 +7,12 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    res.status(200).json(data);
+    res.json(data);
 
     if (!response.ok) {
       throw new Error(`Swiggy Menu API returned status ${response.status}`);
     }
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch menu data" });
+    res.json({ error: "Failed to fetch menu data" });
   }
 }
