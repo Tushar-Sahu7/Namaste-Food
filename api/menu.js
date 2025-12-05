e439c56c7e4fbd987f9772ad6a86f035b1a1e119
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-12-06 04:34:40 +0530

    Fix api handlers

--- api/menu.js
+++ api/menu.js
@@ -1,14 +1,29 @@
 import axios from "axios";
+
 const API_BASE_URL = "https://namastedev.com/api/v1";
 
-export const getRestaurantMenu = async (resId) => {
-try {
-const { data } = await axios.get(
-`${API_BASE_URL}/listRestaurantMenu/${resId}`
-);
-console.log(data);
-return data.data;
-} catch (error) {
-throw error;
-}
+const getRestaurantMenu = async (resId) => {
+  try {
+    const { data } = await axios.get(`${API_BASE_URL}/listRestaurantMenu/${resId}`);
+    console.log("Menu fetched:", data);
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
 };
+
+export default async function handler(req, res) {
+  try {
+    const { resId } = req.query;
+  
+    if (!resId) {
+      return res.status(400).json({ error: "Restaurant ID is required" });
+    }
+
+    const menu = await getRestaurantMenu(resId);
+    return res.status(200).json(menu);
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message || "Server error" });
+  }
+}
--- api/restaurant.js
+++ api/restaurant.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 const API_BASE_URL = "https://namastedev.com/api/v1";
-// Get restaurant list
-export const getRestaurants = async () => {
-try {
-const { data } = await axios.get(`${API_BASE_URL}/listRestaurants`);
-console.log(data);
-return data
-} catch (error) {
-throw error;
-}
+
+const getRestaurants = async () => {
+  try {
+    const { data } = await axios.get(`${API_BASE_URL}/listRestaurants`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
+
+export default async function handler(req, res) {
+  try {
+    const data = await getRestaurants();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
