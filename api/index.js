a7eaf1eae04ab38c192ed6f71f54bf4453d85edd
Tushar-Sahu7 <sahutushar532@gmail.com>
2025-12-06 08:13:00 +0530

    added serverless

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import menuRouter from './menu.js';
 import restaurantRouter from './restaurant.js';
+import serverless from "serverless-http"
 
 const app = express();
 app.use(cors());
@@ -11,4 +12,4 @@ app.use(express.json());
 app.use('/api', menuRouter);
 app.use('/api', restaurantRouter);
 
-export default app;
+export const handler = serverless(app);
